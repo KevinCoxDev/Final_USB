f32eca02cc7ded8e01a54ba2e7db1c8bf4731001
kevc988 <kcox@netgear.com>
2017-05-09 09:42:22 +0100

    Working on single volume single thread

--- src/FormatThread.java
+++ src/FormatThread.java
@@ -1,4 +1,7 @@
-import java.io.IOException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
 
 /**
  * Created by kcox on 08/05/2017.
@@ -6,18 +9,36 @@ import java.io.IOException;
 public class FormatThread {
 
     private Thread t;
+    private String line;
 
     FormatThread() {
         System.out.println("Creating ");
     }
 
-    public void run() throws IOException {
-        String dir = System.getProperty("user.dir");
-        String st = new String(dir);
+    public void run(String size) throws IOException {
+
+        try (Writer writer = new FileWriter("scripts/Output.json")){
+            Gson gson = new GsonBuilder().create();
+            String json = gson.toJson(size);
+            System.out.print(json);
+            gson.toJson(json, writer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        script();
+
+    }
+
+    public void script() throws IOException {
+        String st = System.getProperty("user.dir");
         st = new StringBuffer(st).insert(2, "\\").toString();
         String string=("powershell.exe"+ "\\"+" \""+st+"\\"+"scripts\\"+"format.ps1\"");
         Process ps = Runtime.getRuntime().exec(string);
         ps.getOutputStream().close();
-        System.out.println("WORKS");
+        BufferedReader stdout = new BufferedReader(new InputStreamReader(ps.getInputStream()));
+        while ((line = stdout.readLine()) != null) {
+            System.out.println(line);
+        }
     }
 }
--- src/Powershell.java
+++ src/Powershell.java
@@ -3,6 +3,7 @@ import com.google.gson.GsonBuilder;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Powershell {
@@ -12,11 +13,12 @@ public class Powershell {
 
     public static void main(String [] args) throws IOException {
         Powershell p = new Powershell();
-        System.out.print((p.volumeList()));
-        p.format();
+        p.threadCaller();
     }
 
-    public String volumeList() throws IOException {
+
+
+    public ArrayList volumeList() throws IOException {
         String st = System.getProperty("user.dir");
         st = new StringBuffer(st).insert(2, "\\").toString();
         String string=("powershell.exe"+ "\\"+" \""+st+"\\"+"scripts\\"+"disk_info.ps1\"");
@@ -27,26 +29,17 @@ public class Powershell {
             volumes.add(line);
         }
         stdout.close();
-        Object[] volArray = (volumes.toArray());
-        Gson gson = new GsonBuilder().create();
-        String json = gson.toJson(volArray);
-        for(int i = 0; i<volArray.length; i++) {
-            try (Writer writer = new FileWriter("scripts/Output.json")) {
-                gson.toJson(volArray[i], writer);
-                FormatThread thread = new FormatThread();
-                thread.run();
-            }
+        System.out.print(Arrays.toString(new ArrayList[]{volumes}));
+
+        return volumes;
+    }
+
+    public void threadCaller() throws IOException{
+        ArrayList array = volumeList();
+        FormatThread thread = new FormatThread();
+        for(int i =0 ; i < array.size(); i++){
+            thread.run((String) array.get(i));
         }
-        return json;
-    }
 
-    public void format() throws IOException {
-        String dir = System.getProperty("user.dir");
-        String st = new String(dir);
-        st = new StringBuffer(st).insert(2, "\\").toString();
-        String string=("powershell.exe"+ "\\"+" \""+st+"\\"+"scripts\\"+"format.ps1\"");
-        Process ps = Runtime.getRuntime().exec(string);
-        ps.getOutputStream().close();
     }
-
 }
